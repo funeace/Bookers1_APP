3ce5beca8300f53e74ad92dd268efb6e7bfa15d1
Kengo <funeace@gmail.com>
2020-06-06 14:57:05 +0900

    [Add]初期設定

---
+++ bookers-api/app/controllers/v1/books_controller.rb
@@ -0,0 +1,42 @@
+class V1::BooksController < ApplicationController
+  def index
+    books = Book.all
+    render json: books
+  end
+
+  def show
+    book = Book.find(params[:id])
+    render json: book
+  end
+
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      render json: book
+    else
+      render json: book.errors
+    end
+  end
+
+  def create
+    book = Book.new(book_params)
+    if book.save
+      render json: book, status: :created
+    else
+      render json: book.errors, status: ::unprocessable_entity
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    if book.destroy
+      render json: blog
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:books).permit(:title, :body)
+  end
+end
